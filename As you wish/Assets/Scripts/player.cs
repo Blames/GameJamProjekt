bc11570a0a148f12d33ac74bab633b1d46213246
Gui <ethevegwenola@gmail.com>
2019-01-26 15:43:16 +0100

    player movement

---
+++ As you wish/Assets/Scripts/player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class player : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+    private Vector2 direction;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+    }
+
+    public void Move()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+        }else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        
+    }
+}
